661d310d2704b6b4390ce5a95bb5da251f21c01c
Madhav Malhotra <madhav.malhotra003@gmail.com>
2024-05-10 16:28:56 -0400

    created vectorstore from raw article content

--- create_vectorstore.py
+++ create_vectorstore.py
@@ -14,15 +14,15 @@ import requests
 from tqdm import tqdm
 from dotenv import load_dotenv
 
-from langchain.vectorstores.faiss import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.docstore.document import Document
-from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
+from langchain_voyageai import VoyageAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 # Load API key
 load_dotenv(".env")
-KEY = os.environ["ANTHROPIC_API_KEY"]
+KEY = os.environ["VOYAGE_API_KEY"]
 
 def scrape_data() -> list:
     """ Returns list of website snippets """
@@ -48,63 +48,55 @@ def scrape_data() -> list:
     with open("unprocessed/articles.json", "w") as f:
         json.dump(articles, f, indent=4)
 
-def format_documents(path: str) -> list:
+def format_documents(path: str, chunk_size : int) -> list:
     """ Returns list of processed LangChain Documents """
 
     # Load documents
-    print("Loading documents...")
     articles = json.load(open(path, "r"))
     documents = []
-    none_count = 0
 
-    for b in bios: 
-        if b["bio"] is not None:
-            documents.append(Document(page_content=b["bio"], metadata={
-                "name": b["name"],
-                "position": b["position"],
-                "email": b["email"],
-                "expertise": b["expertise"],
-                "profile": b["profile"],
-            }))
-        else:
-            none_count += 1
+    for article in articles: 
+        content = article.pop("content")
+        documents.append(Document(page_content=content, metadata=article))
     
-    print(f"Loaded {len(documents)} documents. {none_count} documents skipped since bio was None")
+    print(f"Loaded {len(documents)} documents.")
 
     print(f"Splitting {len(documents)} documents...")
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=0)
     fragments = splitter.split_documents(documents)
 
     return fragments
 
-def get_vectorstore(fragments: list, model: HuggingFaceInferenceAPIEmbeddings, 
-                    save_path: str) -> FAISS:
+def get_vectorstore(fragments: list, model: VoyageAIEmbeddings, 
+                    save_path: str, batch_size : int) -> FAISS:
     """ Embeds documents and returns vectorstore """
     
-    # Create vectorstore (400 docs at a time, else HuggingFace API blocks)
+    # Create vectorstore (batch_size docs at a time to respect rate limits)
     print(f"Creating vectorstore with {len(fragments)} documents...")
+    
     dbs = []
-    for i in tqdm(range(0, len(fragments), 400)):
-        db = FAISS.from_documents(fragments[i : i+400], model)
-        db.save_local(f"{save_path}_{i}")
+    for i in tqdm(range(0, len(fragments), batch_size)):
+        db = FAISS.from_documents(fragments[i : i+batch_size], model)
+        db.save_local(f"{save_path}/vectorstore_{i}")
         dbs.append(db)
         time.sleep(5)
 
     # Merge partial vectorstores and save final copy
     for db in tqdm(dbs[1:]):
         dbs[0].merge_from(db)
-    db.save_local(f"{save_path}_merged")
+    dbs[0].save_local(f"{save_path}/vectorstore_merged")
 
 def main():
+    batch_size = 20
+    chunk_size = 5000
+
     # Load embeddings model
-    model = HuggingFaceInferenceAPIEmbeddings(
-        api_key=KEY, model_name="sentence-transformers/all-MiniLM-l6-v2"
-    )
+    model = VoyageAIEmbeddings(voyage_api_key=KEY, model="voyage-large-2", 
+                               batch_size=batch_size)
 
     # Get vectorstore
-    fragments = get_data("data_new/json/bios.json")
-    get_vectorstore(fragments, model, "data_new/faiss/bios_faiss")
+    fragments = format_documents("./unprocessed/articles.json", chunk_size)
+    get_vectorstore(fragments, model, "./processed/", batch_size)
 
 if __name__ == "__main__":
-    #main()
-    #scrape_data()
+    main()
